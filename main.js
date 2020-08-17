a6d8581e123ebe54f9b80174dacd5ea14a326495
Vbahole <gooss22@gmail.com>
2020-08-17 00:58:48 -0400

    looping over all gpx files

--- main.js
+++ main.js
@@ -1,63 +1,85 @@
 let http = require('http');
 let fs = require('fs');
+let path = require('path');
 let appRoot = require('app-root-path');
 let gpxParser = require('gpxparser');
 let convert = require('convert-units');
 let moment = require('moment');
 let AWS = require("aws-sdk");
 
-AWS.config.update({region: 'us-east-1'});
-var docClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+
+// AWS
+AWS.config.update({
+  region: 'us-east-1'
+});
+var docClient = new AWS.DynamoDB.DocumentClient({
+  apiVersion: '2012-08-10'
+});
 const dbTableName = 'Hike';
 
+// gpxParser
 const sampleGPX = `${appRoot}/gpx/mock/Recording1.gpx`;
+const gpxExportsDir = `${appRoot}/gpx/exports`;
 
 
-let gpx = new gpxParser(); //Create gpxParser Object
-gpx.parse(fs.readFileSync(sampleGPX, {encoding:'utf8', flag:'r'})); //parse gpx file from string data
-let totalDistanceMeters = gpx.tracks[0].distance.total; // IN METERS!!
+let dirCont = fs.readdirSync(gpxExportsDir);
+let files = dirCont.filter(function(e) {
+  return e.match(/.*\.(gpx)/ig);
+});
+console.log(`files count - ${files.length}`);
+files.forEach(function(file, index) {
+  let gpx = new gpxParser();
+  let fullPath = path.join(gpxExportsDir, file);
 
-// convert to miles
-let totalDistanceMiles = convert(totalDistanceMeters).from('m').to('mi')
+  console.log(`THE fullPath ----- ${fullPath}`)
+  gpx.parse(fs.readFileSync(fullPath, {
+    encoding: 'utf8',
+    flag: 'r'
+  })); //parse gpx file from string data
+  let totalDistanceMeters = gpx.tracks[0].distance.total; // IN METERS!!
 
-console.log(`metadata.name is ${JSON.stringify(gpx.metadata.name)}`);
-// console.log(`tracks is ${JSON.stringify(gpx.tracks)}`);
-console.log(`first point is ${JSON.stringify(gpx.tracks[0].points[0])}`);
-console.log(`points.length is ${JSON.stringify(gpx.tracks[0].points.length)}`);
-console.log(`last point is ${JSON.stringify(gpx.tracks[0].points[gpx.tracks[0].points.length - 1])}`);
+  // convert to miles
+  let totalDistanceMiles = convert(totalDistanceMeters).from('m').to('mi')
 
-let firstPointTime = moment(gpx.tracks[0].points[0].time);
-let lastPointTime = moment(gpx.tracks[0].points[gpx.tracks[0].points.length - 1].time);
-console.log(`first point time is ${JSON.stringify(gpx.tracks[0].points[0].time)}`);
-console.log(`last point time is ${JSON.stringify(gpx.tracks[0].points[gpx.tracks[0].points.length - 1].time)}`);
+  console.log(`metadata.name is ${JSON.stringify(gpx.metadata.name)}`);
+  // console.log(`tracks is ${JSON.stringify(gpx.tracks)}`);
+  console.log(`first point is ${JSON.stringify(gpx.tracks[0].points[0])}`);
+  console.log(`points.length is ${JSON.stringify(gpx.tracks[0].points.length)}`);
+  console.log(`last point is ${JSON.stringify(gpx.tracks[0].points[gpx.tracks[0].points.length - 1])}`);
 
-// all trails call this total time as opposed to moving time which is typically smaller
-// let duration = moment.utc(moment(lastPointTime).diff(moment(firstPointTime))).format("HH:mm:ss")
-var duration = moment.duration(lastPointTime.diff(firstPointTime));
-var durMinutes = duration.asMinutes();
-console.log(`durMinutes - ${durMinutes}`);
+  let firstPointTime = moment(gpx.tracks[0].points[0].time);
+  let lastPointTime = moment(gpx.tracks[0].points[gpx.tracks[0].points.length - 1].time);
+  console.log(`first point time is ${JSON.stringify(gpx.tracks[0].points[0].time)}`);
+  console.log(`last point time is ${JSON.stringify(gpx.tracks[0].points[gpx.tracks[0].points.length - 1].time)}`);
 
-// again, AllTrails would use moving time for this pace and not total time
-let paceMinPerMiles = durMinutes / totalDistanceMiles;
+  // all trails call this total time as opposed to moving time which is typically smaller
+  // let duration = moment.utc(moment(lastPointTime).diff(moment(firstPointTime))).format("HH:mm:ss")
+  var duration = moment.duration(lastPointTime.diff(firstPointTime));
+  var durMinutes = duration.asMinutes();
+  console.log(`durMinutes - ${durMinutes}`);
 
-// AWS stuff
-var params = {
-  TableName: dbTableName,
-  Item: {
-    'RecordingDate' : firstPointTime.toString(),
-    'Recording' : {
-      'points': gpx.tracks[0].points,
-      'durationMinutes': durMinutes,
-      'paceMinPerMile': paceMinPerMiles
+  // again, AllTrails would use moving time for this pace and not total time
+  let paceMinPerMiles = durMinutes / totalDistanceMiles;
+
+  // AWS stuff
+  var params = {
+    TableName: dbTableName,
+    Item: {
+      'RecordingDate': firstPointTime.toString(),
+      'Recording': {
+        'points': gpx.tracks[0].points,
+        'durationMinutes': durMinutes,
+        'paceMinPerMile': paceMinPerMiles
+      }
     }
-  }
-};
+  };
 
-// Call DynamoDB to add the item to the table
-docClient.put(params, function(err, data) {
-  if (err) {
-    console.log("Error", err);
-  } else {
-    console.log("Success", data);
-  }
+  // Call DynamoDB to add the item to the table
+  docClient.put(params, function(err, data) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Success", data);
+    }
+  });
 });
