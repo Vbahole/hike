49ad75aecca02cb3c9e0d00422405749bf0c58a3
Vbahole <gooss22@gmail.com>
2020-09-05 22:42:27 -0400

    too many hikes, most hikes in one day stat

--- utils/stats.js
+++ utils/stats.js
@@ -25,11 +25,18 @@ const computeStats = async (dbTableName, gpxRecords, itemType = 'consolidate') =
   console.log(`stats computing for ${gpxRecords.length} recordings \n`);
 
   // OVERALL STATS
-  const totalDistance = gpxRecords.reduce((accum,item) => accum + item.totalDistanceMiles, 0);
-  const totalDurationMinutes = gpxRecords.reduce((accum,item) => accum + item.durationMinutes, 0);
+  const totalDistance = gpxRecords.reduce((accum, item) => accum + item.totalDistanceMiles, 0);
+  const totalDurationMinutes = gpxRecords.reduce((accum, item) => accum + item.durationMinutes, 0);
   const totalDurationHours = moment.duration(totalDurationMinutes, 'minutes').asHours();
   const overallPace = totalDurationMinutes / totalDistance;
-  const hikeCount = gpxRecords.reduce((accum,item) => accum + item.hikeCount, 0);
+  const totalHikeCount = gpxRecords.reduce((accum, item) => accum + item.hikeCount, 0);
+
+  const mostHikesInOneDay = gpxRecords.reduce((accum, item) =>
+     ( accum.hikeCount || 0 ) > item.hikeCount ? accum : item, {});
+
+//  const mostHikesInOneDay = gpxRecords.reduce(function (accum, item) {
+//    return (accum.hikeCount || 0) > item.hikeCount ? accum : item;
+//  }, {});
 
   params = {
     TableName: dbTableName,
@@ -40,9 +47,13 @@ const computeStats = async (dbTableName, gpxRecords, itemType = 'consolidate') =
       'totalDurationMinutes': totalDurationMinutes,
       'totalDurationHours': totalDurationHours,
       'overallPaceMinutesPerMile': overallPace,
-      'hikeCount': hikeCount
-    }
-  };
+      'totalHikeCount': totalHikeCount,
+      'mostHikesInOneDay': {
+        'hikes': mostHikesInOneDay.hikeCount,
+        'date': mostHikesInOneDay.r
+        }
+      }
+    };
 
   console.log(`stats overall put params ${JSON.stringify(params)}`);
 
